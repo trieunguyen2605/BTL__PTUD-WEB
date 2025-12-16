41bedb2c825daaee1513006cbe94c4de0356eae3
trieu nguyen <nguyenvantrieu5267@gmail.com>
2025-12-16 21:38:51 +0700

    test

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>BTL_PTUD-WEB</title>
+</head>
+<body>
+  <h1> Bài tập lớn phát triển ứng dụng web </h1>
+</body>
+</html>
