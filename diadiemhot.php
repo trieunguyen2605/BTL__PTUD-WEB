991c28a605bb1de90dab1477473aae94af324694
trieu nguyen <nguyenvantrieu5267@gmail.com>
2025-12-21 16:49:18 +0700

    hotting

--- diadiemhot.php
+++ diadiemhot.php
@@ -4,8 +4,79 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
+<style>
+  .container{
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center ;
+  }
+
+  .places{
+    width: 80%;
+  }
+
+  .title{
+    margin: 0;
+    font-size: 36px;
+  }
+  .hotPlace__list{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap:wrap;
+    
+  }
+  .pl__item{
+    width: 33.3333%;
+  }
+
+  .pl__item:nth-child(2){
+    box-sizing: border-box;
+    margin: 0 5px;
+  }
+
+  .pl__img{
+    display: flex;
+    justify-content: center;
+  }
+
+  .pl__img img{
+    width: 100%;
+    height: 258px;
+    border-radius: 20px;
+  }
+
+  .pl__name{
+    text-align: center;
+  }
+</style>
 <body>
-  <h1>dia diem hot </h1>
+  <div class="container">
+    <div class="places">
+      <section>
+        <h1 class="title"> TOP các địa điểm HOT  </h1>
+        <div class="hotPlace__list ">
+          <?php
+            include('connect.php'); 
+            $sql="SELECT * FROM `dia_diem` where laTop=1";
+            $result = mysqli_query($conn,$sql);
+            while($row = mysqli_fetch_array($result)){
+          ?>
+            <div class="pl__item" onclick="openInfo(<?php echo $row['id']; ?>)">
+              <div class="pl__img">
+                <img src="<?php echo $row['anhDaiDien']; ?>" alt="">
+              </div>
+              <div class="pl__name"><?php echo $row['tenDiaDiem']; ?> </div>
+            </div>
+          <?php
+              }
+          ?>
+        </div>
+      </section>
+    </div>
+  </div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>BTL_PTUD-WEB</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="asset/css/index.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
